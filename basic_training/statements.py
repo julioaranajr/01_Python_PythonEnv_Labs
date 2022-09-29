4d815970f379bd31e320d67bab27b98c00415587
Julio Arana Jr <julio.arana@cloudreach.com>
2022-09-30 00:44:04 +0200

    last update

--- basic_training/statements.py
+++ basic_training/statements.py
@@ -1,13 +1,10 @@
-    temperature = 25
+temperature = int(input("What's the temperature ?"))
 
-    if temperature > 30:
-        print ("It´s too")
-    
-    elif temperature > 20:
-        print ("It´s perfect")
-
-    elif temperature < 20:
-        print ("It´s a bit cold")
-
-    else:
-        print ("It´s ok")
+if temperature > 30:
+    print("It's too hot")
+elif temperature > 20:
+    print("It's perfect")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's freezing")
