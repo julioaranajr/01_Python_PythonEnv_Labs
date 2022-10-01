6470e49f70281957d99970165333bbd868377c94
Julio Arana Jr <113112001+julioaranajr@users.noreply.github.com>
2022-10-01 09:13:06 +0200

    new files to boto3 labs

---
+++ 05_boto3/create_buckets.py
@@ -0,0 +1,13 @@
+#Creating the Connection
+import boto3
+
+s3 = boto3.resource('s3')
+
+#Creating a Bucket
+s3.create_bucket(Bucket='ta-python-labs')
+
+s3.create_bucket(Bucket='ta-python-labs', CreateBucketConfiguration={
+    'LocationConstraint': 'eu-central-1'})
+
+#Storing Data
+s3.Object('ta-pythonlabs', 'hello.txt').put(Body=open('/tmp/hello.txt', 'rb'))
