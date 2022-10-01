259659f5c4f194211f71da228567c781add0268b
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-01 08:48:48 +0200

    my first python program

--- python_labs/hello.py
+++ python_labs/hello.py
@@ -5,5 +5,5 @@
 # Edit your file with
 
 print(
-    "Hello World, this is my first python program created by Julio Arana - AWS Cloud Systems Developer"
+    "Hello World, this is my first python program"
 )
