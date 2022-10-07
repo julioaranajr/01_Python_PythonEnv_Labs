b59c4934d8d709e0d6893b28c2b9da2f97fb016a
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-07 10:02:57 +0200

    Start with Basic Programming

--- 04_Basic_Programing/age.py
+++
@@ -1,5 +0,0 @@
-date = input("Enter the year you were born? ")
-# input will return a string
-age = 2022 - int(date)  # convert the variable to an integer before doing the arithmetic
-
-print(age)
--- 04_Basic_Programing/inputs.py
+++
@@ -1,6 +0,0 @@
-apples = input("How many apples do you have ?")
-price = 0.415
-print("You have " + apples + " apples!")
-
-total = float(apples) * price
-print("Total price in Euros is = " + str(total))
